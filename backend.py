556522eaea7f058a3784f13bb92dfef3589ae6ad
Yining Hong <yhong3@andrew.cmu.edu>
2025-03-28 12:49:58 -0400

    finish lab9

--- backend.py
+++ backend.py
@@ -4,13 +4,15 @@ import socket
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def hello_user():
     user_id = request.args.get("user_id", "Guest")
     instance = socket.gethostname()
-    
-    return f"Hello, {user_id}! This response is from instance:" #Use the above defined instance here 
 
-if __name__ == '__main__':
+    return f"Hello, {user_id}! This response is from instance: {instance}"  # Use the above defined instance here
+
+
+if __name__ == "__main__":
     # TODO: Change the port if needed (default is 5001)
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host="0.0.0.0", port=5001)
